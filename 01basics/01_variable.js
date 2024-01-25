445e2ace760091ed8c23bf345359b4035683fc43
sachin kumar <123663045+sachin3059@users.noreply.github.com>
2024-01-25 08:16:42 +0000

    data_types

---
+++ 01basics/01_variable.js
@@ -0,0 +1,13 @@
+const registrationNo = 22223
+let userEmail = "sachinkum22223@gmail.com"
+var accountPassword = "123456"
+address = "prayagraj"
+
+
+// registrationNo = 22222
+console.log(registrationNo)
+
+userEmail = "ankur@gmail.com"
+address = "kanpur"
+let state;
+console.table([userEmail,address,registrationNo,state])
---
+++ 01basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version of js
+
+//alert("hello") // we are using node js not browser
+console.log(5)
+
+console.log("hello")
+
+
+
+// data type
+// string = ""
+// boolean => true, false
+// null => standalone value
+// undifined 
+// symbol => unique
+
+// object
+// typeof
+console.log(typeof("sachin")) // string
+console.log(typeof(null))  // object
+console.log(typeof(undefined)) // undifined
