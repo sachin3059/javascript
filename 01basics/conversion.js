24deaa595eda4adee26a60e9a8204223c536fc87
sachin kumar <123663045+sachin3059@users.noreply.github.com>
2024-01-25 09:13:13 +0000

    operators

---
+++ 01basics/conversion.js
@@ -0,0 +1,48 @@
+let score = "33"
+console.log(typeof(score))
+
+let value = Number(score)
+console.log(typeof(value))
+console.log(value)
+
+// "33" =>33
+// "33abs" => NaN
+
+
+// **********OPERATIONS********
+
+console.log(5+5)
+console.log(5-5)
+console.log(5*5)
+console.log(5**5)
+console.log(5/5)
+console.log(5%5)
+console.log(5^5)
+console.log(5|5)
+console.log(5&0)
+console.log(!5)
+
+let str1 = "sachin"
+let srt2 = " kumar"
+let str3 = str1 + srt2
+console.log(str3)
+
+// console.log("1"+2) =>12
+// console.log(1+"2")  => 12
+// console.log("1" + "2") =>12
+// console.log("1" + 2 + 2) =>122
+// console.log(2 + 2 + "1")  =>41
+
+
+//****comparison*********
+console.log(2>1)
+console.log(2==1)
+console.log(2<1)
+console.log("2" == 2)
+console.log("2" === 2)
+
+
+
+
+
+
